472f68869840f22bc782dd3ecb29a037a002c8a1
CharlyMech <sanchezreciocarlos99@gmail.com>
2025-10-28 20:20:19 +0100

    fix: try fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
+	site: "https://charlymech.com", // Tu dominio personalizado
 	integrations: [react()],
 	vite: {
 		plugins: [tailwindcss()],
